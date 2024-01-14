534169349c8fceab8a45d7e9784659fe67d4d26b
benlcohn <benlcohn@gmail.com>
2024-01-13 18:02:47 -0800

    controllers back bbI

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const restaurantsCtrl = require('../ghost/restaurants');
+const restaurantsCtrl = require('../controllers/restaurants');
 const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 
