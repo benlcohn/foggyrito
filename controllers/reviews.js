99eb77fc967a909a77453b159b862e193d60e2e5
benlcohn <benlcohn@gmail.com>
2024-01-17 14:07:59 -0800

    Deploy to heroku

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -3,7 +3,9 @@ const Restaurant = require('../models/restaurant');
 
 module.exports = {
     create,
-    remove
+    remove,
+    edit,
+    update
 }
 
 async function create(req, res) {
@@ -23,9 +25,32 @@ async function create(req, res) {
 }
 
 async function remove(req, res) {
-    const restaurant = await Restaurant.findOne({'reviews._id': req.params.id, 'reviews.user': req.user._id});
+    const restaurant = await Restaurant.findOne({'reviews._id': req.params.reviewId, 'reviews.user': req.user._id});
     if (!restaurant) return res.redirect(`/restaurants/${restaurant._id}`);
     restaurant.reviews.remove(req.params.id);
     await restaurant.save();
     res.redirect(`/restaurants/${restaurant._id}`);
+}
+
+async function edit(req, res) {
+    const restaurant = await Restaurant.findOne({'reviews._id': req.params.reviewId, 'reviews.user': req.user._id});
+    const review = restaurant.reviews.id(req.params.reviewId);
+    // render /reviews/edit.ejs
+    res.render('reviews/edit', { title: 'Edit Review', review, restaurant });
+}
+
+async function update(req, res) {
+    const restaurant = await Restaurant.findOne({'reviews._id': req.params.reviewId});
+    const reviewSubdoc = restaurant.reviews.id(req.params.reviewId);
+
+    if (!reviewSubdoc.user.equals(req.user._id)) return res.redirect(`/restaurants/${restaurant._id}`);
+    // Update the text in review
+    review.Subdoc.text = req.body.text;
+    try {
+        await restaurant.save();
+    } catch (err) {
+        console.log(err.message);
+    }
+    // Redirect back to show view
+    res.redirect(`/restaurants/${restaurants._id}`);
 }
--- routes/reviews.js
+++ routes/reviews.js
@@ -5,6 +5,10 @@ const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 // POST for creating new reviews
 router.post('/restaurants/:id/reviews', ensureLoggedIn, reviewsCtrl.create);
-router.delete('/reviews/:id', ensureLoggedIn, reviewsCtrl.remove);
+router.delete('/reviews/:reviewId', ensureLoggedIn, reviewsCtrl.remove);
+//  for editing existing reviews
+router.get('/restaurants/:id/reviews/:reviewId/edit', ensureLoggedIn, reviewsCtrl.edit);
+// PUT to make actual edit
+router.put('/reviews/:reviewId', ensureLoggedIn, reviewsCtrl.update);
 
 module.exports = router;
